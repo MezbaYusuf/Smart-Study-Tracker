84a9a2c8b5b7bb93070f6e7c6597bb9b35d775f3
mezba <tyeaqub@gmail.com>
2026-08-16 17:40:22 +0600

    create main.py and ui ready

---
+++ main.py
@@ -0,0 +1,13 @@
+#ui
+
+print('''========== SMART STUDY PLANNER ==========
+
+1. Add subject
+2. Add study task
+3. View today's tasks
+4. Complete a task
+5. Delete a task
+6. View progress
+7. Search tasks
+8. Save & Exit
+''')

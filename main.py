1018b17b88c5c293840503ace4166561b190d9df
mezba <tyeaqub@gmail.com>
2026-08-16 17:43:55 +0600

    create the basic structure for the project

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-#ui
-
-print('''========== SMART STUDY PLANNER ==========
+while True:
+    print('''========== SMART STUDY PLANNER ==========
 
 1. Add subject
 2. Add study task
@@ -11,3 +10,20 @@ print('''========== SMART STUDY PLANNER ==========
 7. Search tasks
 8. Save & Exit
 ''')
+
+    user_choice = int(input("Enter your choice (1-8): "))
+
+    if user_choice == 1:
+        print("\n[+] Add subject selected.\n")
+        
+    elif user_choice == 2:
+        print("\n[+] Add study task selected.\n")
+        
+    elif user_choice == 8:
+        print("\nExiting... Goodbye!\n")
+        break  
+        
+    else:
+        print("\nInvalid choice! Please try again.\n")
+
+    input("Press Enter to go back to the menu...")
